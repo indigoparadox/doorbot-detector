c67c7e3510cb60b3129254f5fe5c3d40557a0a8d
indigoparadox <indigo.repos@interfinitydynamics.info>
2021-01-15 02:04:17 -0500

    Added video encoding.

--- detector/detector.py
+++ detector/detector.py
@@ -6,6 +6,35 @@ import time
 from datetime import datetime
 from threading import Thread
 
+CAPTURE_NONE = 0
+CAPTURE_VIDEO = 1
+CAPTURE_PHOTO = 2
+
+class CaptureEncoder( Thread ):
+    def __init__( self, path, frames, w, h ):
+        super().__init__()
+
+        self.path = path
+        self.frames = frames
+        self.w = w
+        self.h = h
+
+    def run( self ):
+
+        logger = logging.getLogger( 'detector.capture.encoder' )
+
+        logger.info( 'encoding {} ({} frames)...'.format(
+            self.path, len( self.frames ) ) )
+
+        fourcc = cv2.VideoWriter_fourcc( *'MP4V' )
+        encoder = \
+            cv2.VideoWriter( self.path, fourcc, 20.0, (self.w, self.h) )
+        for frame in self.frames:
+            encoder.write( frame )
+        encoder.release()
+
+        logger.info( 'encoding {} completed'.format( self.path ) )
+
 class Detector( Thread ):
 
     ''' This grabs frames from the camera and detects stuff in them. '''
@@ -33,6 +62,18 @@ class Detector( Thread ):
         self.blur = int( kwargs['blur'] ) if 'blur' in kwargs else 5
         self.threshold = \
             int( kwargs['threshold'] ) if 'threshold' in kwargs else 127
+
+        self.capture = []
+        if 'capture' in kwargs:
+            cap_list = kwargs['capture'].split( ',' )
+            if 'video' in cap_list:
+                self.capture.append( CAPTURE_VIDEO )
+                logger.info( 'capturing videos' )
+            if 'photo' in cap_list:
+                self.capture.append( CAPTURE_PHOTO )
+                logger.info( 'capturing photos' )
+
+        self.capture_frames = []
 
         logger.info( 'saving snapshots to {}'.format( self.snapshots ) )
 
@@ -109,20 +150,39 @@ class Detector( Thread ):
                     self._notify( 'ignored', 'edge {}x{} at {}, {}'.format(
                         w, h, x, y ) )
                 else:
+
+                    timestamp = datetime.now().strftime(
+                        '%Y-%m-%d-%H-%M-%S-%f' )
+                    if CAPTURE_PHOTO in self.capture:
+                        cv2.imwrite( '{}/{}.jpg'.format(
+                            self.snapshots, timestamp ), frame )
+                    if CAPTURE_VIDEO in self.capture:
+                        if 0 == len( self.capture_frames ):
+                            self.capture_timestamp = datetime.now().strftime(
+                                '%Y-%m-%d-%H-%M-%S-%f' )
+                        self.capture_frames.append( frame.copy() )
+
                     # TODO: Vary color based on type of object.
                     # TODO: Send notifier w/ summary of current objects.
                     # TODO: Make this summary retained.
                     # TODO: Send image data.
-                    self._notify( 'movement', '{}x{} at {}, {}'.format(
-                        w, h, x, y ) )
-
-                    timestamp = datetime.now().strftime(
-                        '%Y-%m-%d-%H-%M-%S-%f' )
-                    cv2.imwrite( '{}/{}.jpg'.format(
-                        self.snapshots, timestamp ), frame )
+                    self._notify( 'movement', '{}x{} at {}, {} ({}f)'.format(
+                        w, h, x, y, len( self.capture_frames ) ) )
 
                     color = (255, 0, 0)
                     cv2.rectangle( frame, (x, y), (x + w, y + h), color, 3 )
+            elif 0 < len( self.capture_frames ):
+                if CAPTURE_VIDEO in self.capture:
+                    # Ship the frames off to a separate thread to write out.
+                    encoder = CaptureEncoder( 
+                        '{}/{}.mp4'.format(
+                            self.snapshots,
+                            self.capture_timestamp ),
+                        self.capture_frames,
+                        self.cam.w,
+                        self.cam.h )
+                    self.capture_frames = []
+                    encoder.start()
 
             logger.debug( 'setting frame...' )
             if self.reserver:
