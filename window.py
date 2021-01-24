f067271d796f609959b1e2297525f4919bb8c301
indigoparadox <indigo.repo@interfinitydynamics.info>
2021-01-24 01:01:38 -0500

    Added overlay to window.

--- window.py
+++ window.py
@@ -7,7 +7,8 @@ import io
 from paho.mqtt import client as mqtt_client
 from configparser import ConfigParser
 from tkinter import Tk, Frame, Label
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image, ImageDraw
+from detector.overlay import Overlays, WeatherOverlay
 
 class SnapWindow( Frame ):
 
@@ -21,12 +22,22 @@ class SnapWindow( Frame ):
 
         self.pack()
 
-        img = Image.new( 'RGB', self.snap_size )
-        self.image_tk = ImageTk.PhotoImage( img )
+        self.overlays = kwargs['overlays']
+        self.overlays.start()
 
-        self.image = Label( self, image=self.image_tk )
+        self.overlay = kwargs['winoverlay'] if 'winoverlay' in kwargs else \
+            None
+        self.overlay_coords = \
+            tuple( [int( x ) for x in \
+                kwargs['winoverlaycoords'].split( ',' )] ) \
+            if 'winoverlaycoords' in kwargs else (0, 0)
+
+        self.image = Label( self )
         self.image.pack( fill="both", expand="yes" )
         
+        self.image_pil = Image.new( 'RGB', self.snap_size )
+        self.draw_image( self.image_pil )
+
         self.mqtt = mqtt_client.Client(
             'window-{}'.format( kwargs['uid'] ),
             True, None, mqtt_client.MQTTv31 )
@@ -40,6 +51,43 @@ class SnapWindow( Frame ):
         host_port = (kwargs['host'], int( kwargs['port'] ))
         logger.info( 'connecting to MQTT at {}...'.format( host_port ) )
         self.mqtt.connect( host_port[0], host_port[1] )
+        
+        self.update_overlays()
+
+    def draw_overlay( self, img ):
+
+        if not self.overlay:
+            return
+
+        text = self.overlay.split( "\\n" )
+        org = self.overlay_coords
+        draw = ImageDraw.Draw( img )
+        for line in text:
+            line = self.overlays.line( line )
+            draw.text( org, line, fill=(255, 255, 255) )
+            org = (org[0], org[1] + 20)
+
+        return img
+
+    def draw_image( self, image ):
+        image_tk = ImageTk.PhotoImage( image )
+        self.image.configure( image=image_tk )
+        self.image_tk = image_tk
+
+    def update_overlays( self ):
+
+        logger = logging.getLogger( 'window.overlays' )
+
+        if not self.overlay:
+            return
+
+        logger.debug( 'updating...' )
+
+        img = self.draw_overlay( self.image_pil.copy() )
+
+        self.draw_image( img )
+
+        self.after( 1000, self.update_overlays )
 
     def on_connected( self, client, userdata, flags, rc ):
         logger = logging.getLogger( 'window.connected' )
@@ -50,11 +98,9 @@ class SnapWindow( Frame ):
     def on_received( self, client, userdata, message ):
         logger = logging.getLogger( 'window.received' )
         logger.debug( 'snap received ({} kB)'.format( len( message.payload ) ) )
-        img = Image.open( io.BytesIO( message.payload ) )
-        img = img.resize( self.snap_size )
-        image_tk = ImageTk.PhotoImage( img )
-        self.image.configure( image=image_tk )
-        self.image_tk = image_tk
+        image_raw = Image.open( io.BytesIO( message.payload ) )
+        self.image_pil = image_raw.resize( self.snap_size )
+        self.draw_image( self.image_pil )
 
     def stop( self ):
         logger = logging.getLogger( 'window.stop' )
@@ -72,6 +118,7 @@ def main():
     if args.verbose:
         level = logging.DEBUG
     logging.basicConfig( level=level )
+    logger = logging.getLogger( 'main' )
     
     config = ConfigParser()
     config.read( args.config )
@@ -80,7 +127,16 @@ def main():
     root.title( 'Camera Activity' )
     root.attributes( '-topmost', True )
 
+    overlay_thread = Overlays()
+
+    try:
+        weather_cfg = dict( config.items( 'weather' ) )
+        overlay_thread.overlays.append( WeatherOverlay( **weather_cfg ) )
+    except Exception as e:
+        logger.error( e )
+
     win_cfg = dict( config.items( 'mqtt' ) )
+    win_cfg['overlays'] = overlay_thread
 
     win = SnapWindow( root, **win_cfg )
 
