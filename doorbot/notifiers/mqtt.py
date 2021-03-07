c33ae1f43dcb64174789188660958bad1ab69f6c
indigoparadox <indigo.repo@interfinitydynamics.info>
2021-03-06 20:21:47 -0500

    Use common logger for mqtt.

--- doorbot/notifiers/mqtt.py
+++ doorbot/notifiers/mqtt.py
@@ -12,7 +12,7 @@ class MQTTNotifier( Notifier ):
 
         super().__init__( **kwargs )
 
-        logger = logging.getLogger( 'mqtt.init' )
+        self.logger = logging.getLogger( 'mqtt' )
 
         self.snapshots = True if 'snapshots' in kwargs \
             and 'true' == kwargs['snapshots'] else False
@@ -28,36 +28,32 @@ class MQTTNotifier( Notifier ):
             self.mqtt.tls_set(
                 kwargs['ca'], tls_version=ssl.PROTOCOL_TLSv1_2 )
         self.mqtt.on_connect = self.on_connected
-        logger.info( 'connecting to MQTT at %s:%d...',
+        self.logger.info( 'connecting to MQTT at %s:%d...',
             kwargs['host'], int( kwargs['port'] ) )
         self.mqtt.connect( kwargs['host'], int( kwargs['port'] ) )
 
     def send( self, subject, message ):
-        logger = logging.getLogger( 'mqtt.send' )
         topic = '{}/{}'.format( self.topic, subject )
-        logger.debug( 'publishing %s to %s...', message, topic )
+        self.logger.debug( 'publishing %s to %s...', message, topic )
         self.mqtt.publish( topic, message )
 
     def snapshot( self, subject, attachment ):
-        logger = logging.getLogger( 'mqtt.snapshot' )
         if not self.snapshot:
             return
         topic = '{}/{}'.format( self.topic, subject )
         sz = round( len( attachment ) / 1024, 2 )
-        logger.debug( 'snapshot (%dkB) to %s...', sz, topic )
+        self.logger.debug( 'snapshot (%dkB) to %s...', sz, topic )
         self.mqtt.publish( topic, attachment, retain=True )
         self.mqtt.publish(
             '{}/timestamp'.format( topic ), str( time.time() ), retain=True )
 
     def on_connected( self, client, userdata, flags, rc ):
-        logger = logging.getLogger( 'mqtt.connected' )
-        logger.info( 'mqtt connected' )
+        self.logger.info( 'mqtt connected' )
         self.send( 'error', 'online' )
         self.mqtt.will_set( '{}/error'.format( self.topic ), 'died' )
 
     def stop( self ):
-        logger = logging.getLogger( 'mqtt.stop' )
-        logger.info( 'mqtt shutting down...' )
+        self.logger.info( 'mqtt shutting down...' )
         self.mqtt.disconnect()
         self.mqtt.loop_stop()
 
